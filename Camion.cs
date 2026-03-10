313aa5712ba57a21dfaedac39337b36535e99499
Tabara Nicholas <nicholas.tabara@student.usv.ro>
2026-03-10 11:57:42 +0200

    Am realizat citirea de la tastatura si am organizat tot intr-un meniu

--- Camion.cs
+++ Camion.cs
@@ -28,5 +28,4 @@ namespace Dispecerat
             StatusCurent = StatusCamion.InService;
         }
     }
-
 }
--- Program.cs
+++ Program.cs
@@ -1,32 +1,79 @@
-﻿namespace Dispecerat
+﻿using Dispecerat;
+
+namespace Dispecerat
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("===Sistem Dispecerat Flota ===\n");
-            Camion camion1= new Camion("B-123-TRK","Volvo",20.5);
-            Sofer sofer1 = new Sofer("Popescu", "Ion", "EMP001");
-            Console.WriteLine("--- 1.Status Initial ---");
-            Console.WriteLine($"Camion {camion1.NumarInmatriculare} - Status: {camion1.StatusCurent}");
-            Console.WriteLine($"Sofer {sofer1.Nume} {sofer1.Prenume} - Status: {sofer1.StatusCurent}");
-            Cursa cursa1 = new Cursa("Bucuresti", "Cluj-Napoca", camion1, sofer1);
-            Console.WriteLine("--- 2.Cursa a fost creata ---");
-            Console.WriteLine($"Traseu: {cursa1.PunctPlecare} -> {cursa1.Destinatie}");
-            Console.WriteLine($"Status Cursă: {cursa1.Status}\n");
-            Console.WriteLine("--- 3. Dispecerul porneste cursa ---");
-            cursa1.IncepeCursa();
-            Console.WriteLine($"Status Cursă: {cursa1.Status}");
-            Console.WriteLine($"Status Camion alocat: {camion1.StatusCurent}");
-            Console.WriteLine($"Status Sofer alocat: {sofer1.StatusCurent}\n");
-            Console.WriteLine("--- 4. Cursa a ajuns la destinatie ---");
-            cursa1.FinalizeazaCursa();
-            Console.WriteLine($"Status Cursă: {cursa1.Status}");
-            Console.WriteLine($"Status Camion eliberat: {camion1.StatusCurent}"); 
-            Console.WriteLine($"Status Sofer eliberat: {sofer1.StatusCurent}\n");
+            Camion[] flota = new Camion[100];
+            int numarCamioane = 0;
+            bool ruleaza = true;
+            while (ruleaza=true)
+            {
+                Console.WriteLine("\n=== Meniu Dispecerat Flota ===");
+                Console.WriteLine("1. Adauga un camion nou");
+                Console.WriteLine("2. Afiseaza toate camioanele");
+                Console.WriteLine("3. Cauta un camion (dupa numarul de inmatriculare)");
+                Console.WriteLine("0. Iesire");
+                Console.Write("Alege o optiune: ");
 
-            Console.WriteLine("Apasă tasta Enter pentru a închide...");
-            Console.ReadLine();
+                string optiune = Console.ReadLine();
+                Console.WriteLine();
+                switch (optiune)
+                {
+                    case "1":
+                        Console.Write("Introdu numarul de inmatriculare (ex: B-10-TRK): ");
+                        string numar = Console.ReadLine();
+                        Console.Write("Introdu marca (ex: Volvo): ");
+                        string marca = Console.ReadLine();
+                        Console.Write("Introdu capacitatea in tone (ex: 20): ");
+                        double capacitate = Convert.ToDouble(Console.ReadLine());
+                        flota[numarCamioane] = new Camion(numar, marca, capacitate);
+                        numarCamioane++;
+                        Console.WriteLine(">> Camion adaugat cu succes!");
+                        break;
+                    case "2":
+                        if (numarCamioane == 0)
+                        {
+                            Console.WriteLine("Flota este goala. Nu exista camioane de afisat.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("--- Lista camioane ---");
+                            for (int i = 0; i < numarCamioane; i++)
+                            {
+                                Console.WriteLine($"{i + 1}. Numar {flota[i].NumarInmatriculare} | Marca {flota[i].Marca}, poate transporta {flota[i].CapacitateTone} tone si momentan este {flota[i].StatusCurent}.");
+                            }
+                        }
+                        break;
+                    case "3":
+                        Console.Write("Introdu numarul de inmatriculare pe care il cauti: ");
+                        string numarCautat = Console.ReadLine();
+                        bool gasit = false;
+                        for (int i = 0;i < numarCamioane;i++)
+                        {
+                            if (flota[i].NumarInmatriculare == numarCautat)
+                            {  
+                                Console.WriteLine($"Camion gasit: Marca {flota[i].Marca},poate transporta {flota[i].CapacitateTone} tone si momentan este {flota[i].StatusCurent}.");
+                                gasit = true;
+                                break; 
+                            }
+                        }
+                        if(! gasit)
+                        {
+                            Console.WriteLine("Camionul nu a fost gasit in sistem.");
+                        }
+                        break;
+                    case "0":
+                        ruleaza = false;
+                        Console.WriteLine("Inchidere program.. ");
+                        break;
+                    default:
+                        Console.WriteLine("Optiune invalida! Alege 1, 2, 3, sau 0.");
+                        break;
+                }
+            }
         }
     }
 }
