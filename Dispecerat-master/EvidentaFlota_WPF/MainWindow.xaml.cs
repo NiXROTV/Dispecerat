e93becc35566bc25bf1e41f19a9e7d461b3ee20c
Tabara Nicholas <nicholas.tabara@student.usv.ro>
2026-04-06 12:20:03 +0300

    Laborator 6

---
+++ Dispecerat-master/EvidentaFlota_WPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using LibrarieModele; 
+
+namespace EvidentaFlota_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Creăm un obiect complet
+            Camion c = new Camion("B-123-WPF", "Volvo FH16", 24.5);
+            c.StatusCurent = StatusCamion.Disponibil;
+            c.Dotari = DotariCamion.AerConditionat | DotariCamion.Navigatie | DotariCamion.Dormitor;
+
+            // Afișăm totul frumos (folosim \n pentru rând nou)
+            lblDetalii.Text = $"DETALII VEHICUL:\n" +
+                              $"--------------------------\n" +
+                              $"Număr: {c.NumarInmatriculare}\n" +
+                              $"Marcă: {c.Marca}\n" +
+                              $"Capacitate: {c.CapacitateTone} tone\n" +
+                              $"Status: {c.StatusCurent}\n" +
+                              $"Dotări: {c.Dotari}";
+
+            lblDetalii.Foreground = System.Windows.Media.Brushes.Black;
+        }
+
+    }
+}
